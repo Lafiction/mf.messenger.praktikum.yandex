a9db0bfa172970714c6694cc47bff0baf2a42dc4
Elena Kolesnik <miss.kolesnik@yandex.ru>
2020-12-11 00:24:06 +0300

    Add registration page validation.

--- src/pages/registration.ts
+++ src/pages/registration.ts
@@ -1,4 +1,5 @@
 import { attachCollector } from '../common/formDataCollector.js';
+import { validateFormInput } from '../common/validation.js';
 import { Block } from '../common/block.js';
 import { TextField } from '../components/textField.js';
 import { SubmitBtn } from '../components/submitBtn.js';
@@ -6,73 +7,121 @@ import { SubmitBtn } from '../components/submitBtn.js';
 const Handlebars = (window as any)['Handlebars'];
 
 class RegistrationPage extends Block {
+  private nameFieldComponent: TextField;
+  private lastNameFieldComponent: TextField;
+  private loginFieldComponent: TextField;
+  private emailFieldComponent: TextField;
+  private phoneFieldComponent: TextField;
+  private passwordComponent: TextField;
+  private repeatPasswordComponent: TextField;
+  private submitBtnComponent: SubmitBtn;
+
   constructor() {
     super('form');
   }
 
   componentDidMount() {
+    this.nameFieldComponent = new TextField({
+      fieldType: 'text',
+      fieldName: 'first_name',
+      placeholder: 'Имя'
+    });
+    
+    this.lastNameFieldComponent = new TextField({
+      fieldType: 'text',
+      fieldName: 'last_name',
+      placeholder: 'Фамилия'
+    });
+
+    this.loginFieldComponent = new TextField({
+      fieldType: 'text',
+      fieldName: 'login',
+      placeholder: 'Логин',
+      required: true
+    });
+
+    this.emailFieldComponent = new TextField({
+      fieldType: 'email',
+      fieldName: 'email',
+      placeholder: 'Почта',
+      required: true
+    });
+
+    this.phoneFieldComponent = new TextField({
+      fieldType: 'tel',
+      fieldName: 'phone',
+      placeholder: 'Телефон'
+    });
+
+    this.passwordComponent = new TextField({
+      fieldType: 'password',
+      fieldName: 'password',
+      placeholder: 'Пароль',
+      required: true
+    });
+
+    this.repeatPasswordComponent = new TextField({
+      fieldType: 'password',
+      fieldName: 'repeat_password',
+      placeholder: 'Повторите пароль',
+      required: true
+    });
+
+    this.submitBtnComponent = new SubmitBtn({ value: 'Зарегистрироваться' });
+
+    this.nameFieldComponent.eventBus().on(TextField.EVENTS.FLOW_RENDER, () => {
+      this.eventBus().emit(TextField.EVENTS.FLOW_RENDER);
+    });
+
+    this.lastNameFieldComponent.eventBus().on(TextField.EVENTS.FLOW_RENDER, () => {
+      this.eventBus().emit(TextField.EVENTS.FLOW_RENDER);
+    });
+
+    this.loginFieldComponent.eventBus().on(TextField.EVENTS.FLOW_RENDER, () => {
+      this.eventBus().emit(TextField.EVENTS.FLOW_RENDER);
+    });
+
+    this.emailFieldComponent.eventBus().on(TextField.EVENTS.FLOW_RENDER, () => {
+      this.eventBus().emit(TextField.EVENTS.FLOW_RENDER);
+    });
+
+    this.phoneFieldComponent.eventBus().on(TextField.EVENTS.FLOW_RENDER, () => {
+      this.eventBus().emit(TextField.EVENTS.FLOW_RENDER);
+    });
+
+    this.passwordComponent.eventBus().on(TextField.EVENTS.FLOW_RENDER, () => {
+      this.eventBus().emit(TextField.EVENTS.FLOW_RENDER);
+    });
+
+    this.repeatPasswordComponent.eventBus().on(TextField.EVENTS.FLOW_RENDER, () => {
+      this.eventBus().emit(TextField.EVENTS.FLOW_RENDER);
+    });
+
+    this.submitBtnComponent.eventBus().on(SubmitBtn.EVENTS.FLOW_RENDER, () => {
+      this.eventBus().emit(SubmitBtn.EVENTS.FLOW_RENDER);
+    });
+
     attachCollector(this.element as HTMLFormElement);
+    
+    if (this.element) {
+      validateFormInput(this.element as HTMLFormElement, 'login', /^[a-zа-я0-9_]+$/i);
+      validateFormInput(this.element as HTMLFormElement, 'password', /^.{8,}$/i);
+      validateFormInput(this.element as HTMLFormElement, 'repeat_password', /^.{8,}$/i);
+    }
   }
 
   render() {
     this.element.classList.add('form');
     this.element.setAttribute('action', '#');
 
-    const nameFieldComponent = new TextField({
-      fieldType: 'text',
-      fieldName: 'first_name',
-      placeholder: 'Имя'
-    });
-    const nameField = nameFieldComponent.getContent().outerHTML;
-
-    const lastNameFieldComponent = new TextField({
-      fieldType: 'text',
-      fieldName: 'last_name',
-      placeholder: 'Фамилия'
-    });
-    const lastNameField = lastNameFieldComponent.getContent().outerHTML;
-
-    const loginFieldComponent = new TextField({
-      fieldType: 'text',
-      fieldName: 'login',
-      placeholder: 'Логин',
-      required: true
-    });
-    const loginField = loginFieldComponent.getContent().outerHTML;
-
-    const emailFieldComponent = new TextField({
-      fieldType: 'email',
-      fieldName: 'email',
-      placeholder: 'Почта',
-      required: true
-    });
-    const emailField = emailFieldComponent.getContent().outerHTML;
-
-    const phoneFieldComponent = new TextField({
-      fieldType: 'tel',
-      fieldName: 'phone',
-      placeholder: 'Телефон'
-    });
-    const phoneField = phoneFieldComponent.getContent().outerHTML;
-
-    const passwordComponent = new TextField({
-      fieldType: 'password',
-      fieldName: 'password',
-      placeholder: 'Пароль',
-      required: true
-    });
-    const passwordField = passwordComponent.getContent().outerHTML;
-
-    const repeatPasswordComponent = new TextField({
-      fieldType: 'password',
-      fieldName: 'repeat_password',
-      placeholder: 'Повторите пароль',
-      required: true
-    });
-    const repeatPasswordField = repeatPasswordComponent.getContent().outerHTML;
-
-    const submitBtnComponent = new SubmitBtn({ value: 'Зарегистрироваться' });
-    const submitBtn = submitBtnComponent.getContent().outerHTML;
+    const nameField = this.nameFieldComponent.getContent().outerHTML;
+    const lastNameField = this.lastNameFieldComponent.getContent().outerHTML;
+    const loginField = this.loginFieldComponent.getContent().outerHTML;
+    const emailField = this.emailFieldComponent.getContent().outerHTML;
+    const phoneField = this.phoneFieldComponent.getContent().outerHTML;
+    const passwordField = this.passwordComponent.getContent().outerHTML;
+    const repeatPasswordField = this.repeatPasswordComponent.getContent().outerHTML;
+    const submitBtn = this.submitBtnComponent.getContent().outerHTML;
 
     const pageContent = `
       <form class="form" action="#">
@@ -84,14 +133,26 @@ class RegistrationPage extends Block {
           {{{ lastNameField }}}
         
           {{{ loginField }}}
+
+          <div class='input-requirements login-validation-msg'>
+            Логин должен состоять только из букв, цифр и знаков '_'.
+          </div>
         
           {{{ emailField }}}
         
           {{{ phoneField }}}
         
           {{{ passwordField }}}
+
+          <div class='input-requirements password-validation-msg'>
+            Длина пароля должна быть не меньше 8 символов.
+          </div>
         
           {{{ repeatPasswordField }}}
+
+          <div class='input-requirements repeat_password-validation-msg'>
+            Длина пароля должна быть не меньше 8 символов.
+          </div>
         
           {{{ submitBtn }}}
         
