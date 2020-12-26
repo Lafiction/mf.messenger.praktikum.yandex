ba56f9dddf76db772e2e72cf853de94571d303cc
Elena Kolesnik <miss.kolesnik@yandex.ru>
2020-12-26 19:57:20 +0300

    Add classes route & router

---
+++ src/common/route.ts
@@ -0,0 +1,48 @@
+import { Block } from './block.js';
+
+export class Route {
+  private _pathname: string;
+  private _pageClass: new () => Block<{}>;
+  private _page: null | Block<{}>;
+  private _props: { rootQuery: string };
+
+  constructor(pathname: string, pageClass: new () => Block<{}>, props: { rootQuery: string }) {
+    this._pathname = pathname;
+    this._pageClass = pageClass;
+    this._page = null;
+    this._props = props;
+  }
+
+  navigate(pathname: string) {
+    if (this.match(pathname)) {
+      this._pathname = pathname;
+      this.render();
+    }
+  }
+
+  leave() {
+    if (this._page) {
+      this._page.hide();
+    }
+  }
+
+  match(pathname: string) {
+    return pathname === this._pathname;
+  }
+
+  render() {
+    if (!this._page) {
+      this._page = new this._pageClass();
+
+      const root = document.querySelector(this._props.rootQuery);
+
+      if (root) {
+        root.appendChild(this._page.getContent());
+      }
+            
+      return;
+    }
+
+    this._page.show();
+  }
+} 
