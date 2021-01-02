709e48c079e8d14293c3cf665680035cd64b33fd
Elena Kolesnik <miss.kolesnik@yandex.ru>
2021-01-02 19:37:08 +0300

    Add chat user add/delete buttons to chatProfile component.

--- src/components/chatProfile.ts
+++ src/components/chatProfile.ts
@@ -5,9 +5,17 @@ export class ChatProfile extends Block<{}> {
     super('div', {});
   }
 
-  render() {
+  componentDidMount() {
     this.element.classList.add('chat__profile');
+  }
+
+  render() {
     return `<img class="chat__img" src="http://placekitten.com/50/50" alt="">
-            <p class="chat__name">Brennan Stokes</p>`;
+            <p class="chat__name">Brennan Stokes</p>
+            <div class="chat_user_controls">
+              <span class="add_chat_user">+👤</span>
+              <span class="delete_chat_user">−👤</span>
+            </div>
+            `;
   }
 }
