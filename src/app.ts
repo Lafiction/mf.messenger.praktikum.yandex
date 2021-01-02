8c606c45462611cea8b556efb15d19fefd03641a
Elena Kolesnik <miss.kolesnik@yandex.ru>
2021-01-02 21:34:10 +0300

    Remove chat page.

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,6 @@ import { IndexPage } from './pages/index.js';
 import { RegistrationPage } from './pages/registration.js';
 import { ChangeDataPage } from './pages/changedata.js';
 import { ChangePasswordPage } from './pages/changepassword.js';
-import { ChatPage } from './pages/chat.js';
 import { MessengerPage } from './pages/messenger.js';
 import { Page404 } from './pages/page404.js';
 import { Page500 } from './pages/page500.js';
@@ -16,7 +15,6 @@ router
   .use("/registration", RegistrationPage)
   .use("/changedata", ChangeDataPage)
   .use("/changepassword", ChangePasswordPage)
-  .use("/chat", ChatPage)
   .use("/messenger", MessengerPage)
   .use("/page404", Page404)
   .use("/page500", Page500)
