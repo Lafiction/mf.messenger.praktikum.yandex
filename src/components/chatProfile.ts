6f9ecc4a235044039018b5b7b22a8732c140eadc
Elena Kolesnik <miss.kolesnik@yandex.ru>
2021-01-02 20:25:18 +0300

    Display actual chat avatar and title in chatProfile component.

--- src/components/chatProfile.ts
+++ src/components/chatProfile.ts
@@ -1,8 +1,15 @@
 import { Block } from '../common/block.js';
 
-export class ChatProfile extends Block<{}> {
-  constructor() {
-    super('div', {});
+const Handlebars = (window as any)['Handlebars'];
+
+interface ChatProfileProps {
+  chatAvatar?: string;
+  chatTitle?: string;
+}
+
+export class ChatProfile extends Block<ChatProfileProps> {
+  constructor(props: ChatProfileProps = {}) {
+    super('div', props);
   }
 
   componentDidMount() {
@@ -10,12 +17,27 @@ export class ChatProfile extends Block<{}> {
   }
 
   render() {
-    return `<img class="chat__img" src="http://placekitten.com/50/50" alt="">
-            <p class="chat__name">Brennan Stokes</p>
-            <div class="chat_user_controls">
-              <span class="add_chat_user">+👤</span>
-              <span class="delete_chat_user">−👤</span>
-            </div>
-            `;
+    let avatar = 'http://placekitten.com/50/50';
+    if (this.props.chatAvatar) {
+      avatar = 'https://ya-praktikum.tech/' + this.props.chatAvatar;
+    }
+
+    const content = `
+      <img class="chat__img" src="{{ avatar }}" alt="">
+      <p class="chat__name">{{ title }}</p>
+      <div class="chat_user_controls">
+        <span class="add_chat_user">+👤</span>
+        <span class="delete_chat_user">−👤</span>
+      </div>
+    `;
+
+    const template = Handlebars.compile(content);
+
+    const htmlContent = template({
+      avatar: avatar,
+      title: this.props.chatTitle
+    });
+
+    return htmlContent;
   }
 }
