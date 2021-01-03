00bbe288c7b80afaf0ebd70cb0e4d630aae98cae
Elena Kolesnik <miss.kolesnik@yandex.ru>
2021-01-03 11:37:28 +0300

    Add test for submitBtn.

---
+++ test/submitBtn.spec.ts
@@ -0,0 +1,20 @@
+import { expect } from 'chai';
+import { SubmitBtn } from '../src/components/submitBtn';
+
+describe('SubmitBtn component', () => {
+  describe('when called without actual values', () => {
+    it('should generate correct html', () => {
+      const submitBtn = new SubmitBtn({ value: ''});
+      const html = submitBtn.getContent().outerHTML;
+      expect(html).to.equal(`<input type="submit" value="">`);
+    });
+  });
+
+  describe('when called without not empty value', () => {
+    it('should generate correct html', () => {
+      const submitBtn = new SubmitBtn({ value: 'test'});
+      const html = submitBtn.getContent().outerHTML;
+      expect(html).to.equal(`<input type="submit" value="test">`);
+    });
+  });
+});
